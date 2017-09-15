94f34de76219414e5e65034abef775a3da4cef03
zhidadame <742820195@qq.com>
2017-09-15 14:48:31 +0800

    First commit.

---
+++ weixin-01/src/com/bwie/weixin/servlet/CoreServlet.java
@@ -0,0 +1,44 @@
+package com.bwie.weixin.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bwie.weixin.util.SignUtil;
+
+/**
+ * Servlet implementation class CoreServlet
+ */
+@WebServlet(name = "coreServlet", urlPatterns = { "/coreServlet" })
+public class CoreServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String signature = request.getParameter("signature");
+		String timestamp = request.getParameter("timestamp");
+		String nonce = request.getParameter("nonce");
+		String echostr = request.getParameter("echostr");
+		PrintWriter writer = response.getWriter();
+		
+		if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+			writer.println(echostr);
+		}
+		writer.flush();
+		writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
---
+++ weixin-01/src/com/bwie/weixin/util/SignUtil.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.bwie.weixin.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+/**
+ * @function
+ * @author 王治
+ * @date 2017年6月16日
+ */
+public class SignUtil {
+	private static String token = "yd1503cTest";
+
+	public static boolean checkSignature(String signature, String timestamp, String nonce) {
+		boolean result = false;
+		// 对token、timestamp和nonce按字典排序
+		String[] array = new String[] { token, timestamp, nonce };
+		Arrays.sort(array);
+
+		// 将三个参数字符串拼接成一个字符串
+		String str = array[0].concat(array[1]).concat(array[2]);
+
+		// 对拼接后的字符串进行sha1加密
+		MessageDigest md = null;
+		String byte2str = null;
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		//将拼接后的字符串使用byte数组进行sha1加密
+		byte[] digest = md.digest(str.getBytes());
+		//将byte数组转换成字符串
+		byte2str = byte2str(digest);  
+		if (byte2str != null && byte2str.equals(signature)) {
+			result = true;
+		}
+		return result;
+	}
+
+	/**
+	 * 将字节数组转换成字符串
+	 * 
+	 * @param array
+	 *            字节数组
+	 * @return String
+	 */
+	public static String byte2str(byte[] array) {
+		StringBuffer hexstr = new StringBuffer();
+		String shaHex = "";
+		for (int i = 0; i < array.length; i++) {
+			shaHex = Integer.toHexString(array[i] & 0xFF);
+			if (shaHex.length() < 2) {
+				hexstr.append(0);
+			}
+			hexstr.append(shaHex);
+		}
+		return hexstr.toString();
+	}
+
+}
